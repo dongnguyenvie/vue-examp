7918412ba2e3d8a832a654b7b27b2a3a9c651c1e
hd.dong.nguyen <dong.nguyen@hdwebsoft.co>
2019-02-13 17:03:48 +0700

    config deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname)+"/dist"))
-app.use('/dist',express.static(__dirname + "/dist"));
+// app.use('/dist',express.static(__dirname + "/dist"));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html')
